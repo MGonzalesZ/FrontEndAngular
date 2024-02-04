c855854241b4a9642232d4bde7d2c56f16df5e4e
MGonzalesZ <mauriciogzb1322@gmail.com>
2024-02-04 17:15:36 -0400

    Creacion de componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatosComponent } from './datos/datos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatosComponent],
+  // template: `<h1>Mi primer proyecto en Angular</h1>
+  //   <p>Hecho por: Mauricio Gonzales</p>`,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-1';
+  autor = 'Mauricio Gonzales Zubieta';
+  registrado = true;
 }
---
+++ src/app/datos/datos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'datitos',
+  standalone: true,
+  imports: [],
+  templateUrl: './datos.component.html',
+  styleUrl: './datos.component.css',
+})
+export class DatosComponent {}
